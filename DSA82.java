9d83088e53b22b4bf04d22d536994adee3ea55ba
RonaldoN71 <ronaldonandeibam7@gmail.com>
2025-01-16 09:58:13 +0530

    new  file

--- DSA82.java
+++ DSA82.java
@@ -22,31 +22,52 @@
 //     }
 // }
 
-import java.util.Arrays;
+// import java.util.Arrays;
 
-public class DSA82{
-    static int FindMaj(int a[] ){
-        Arrays.sort(a);
+// public class DSA82{
+//     static int FindMaj(int a[] ){
+//         Arrays.sort(a);
+//         int n = a.length;
+//         if(n==1){
+//             return a[0];
+//         }
+//         int count = 1;
+//         for(int i=1;i<n;i++){
+//             if(a[i-1]==a[i]){
+//                 count++;
+//             }else{
+//                 if(count>n/2){
+//                     return a[i-1];
+//                 }
+//                 count = 1;
+//             }
+//             if(count>n/2){
+//                 return a[i];
+//             }
+//         }
+//         return -1;
+        
+//     }
+//     public static void main(String[]args){
+//         int a[] = { 1, 2, 1, 3, 5, 1,1};
+//         System.out.println(FindMaj(a));
+//     }
+// }
+
+import java.util.HashMap;
+
+class DSA82{
+    static int FindMaj(int a[]){
         int n = a.length;
-        if(n==1){
-            return a[0];
-        }
-        int count = 1;
-        for(int i=1;i<n;i++){
-            if(a[i-1]==a[i]){
-                count++;
-            }else{
-                if(count>n/2){
-                    return a[i-1];
-                }
-                count = 1;
-            }
-            if(count>n/2){
-                return a[i];
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int val: a){
+            map.put(val,map.getOrDefault(val,0)+1);
+            if(map.get(val)>n/2){
+                return val;
             }
         }
         return -1;
-        
     }
     public static void main(String[]args){
         int a[] = { 1, 2, 1, 3, 5, 1,1};
